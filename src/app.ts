63ec3c06a5680e6269e10758e571b9fdeb833b6c
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-06-16 02:12:26 +0400

    add new routes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,34 @@
-import cors, { CorsOptions } from 'cors';
 import dotenv from "dotenv";
-import express, { Express } from "express";
 import bodyParser from 'body-parser';
+import cors, { CorsOptions } from 'cors';
+import express, { Express, Response, Request } from "express";
+import cookieParser from "cookie-parser";
 
-import userRouter from './routes/userRoute';
+import userRouter from './routes/userRouter';
+import authRouter from './routes/authRouter';
+import refreshRouter from './routes/refreshRouter';
+import { authMiddleware } from "./middlewares/authMiddleware";
 
 dotenv.config();
 
 const app: Express = express();
 
 const corsOptions: CorsOptions = {
-    origin: "http://localhost:3000"
+    origin: "http://localhost:3000",
+    credentials: true
 };
 
+app.use(cookieParser());
+app.use(express.json());
+// app.use(bodyParser.json());
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
+
+app.use('/auth', authRouter);
 app.use('/users', userRouter);
+app.use('/refresh', refreshRouter);
+
+app.get('/protected', authMiddleware, (req, res) => {
+    res.json({ message: 'This is a protected route' });
+});
 
 export default app;
