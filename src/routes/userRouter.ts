d310e9e45b491aa6ad2c12289cd858adc4453aeb
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-06-16 02:14:15 +0400

    change filename

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,15 @@
 import { createUser, deleteUser, getUser, getUsers, updateUser } from "../controllers/userController";
 import { Router } from "express";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router: Router = Router();
 
 router.get('/', getUsers);
-router.get('/:id', getUser);
 router.post('/', createUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
+
+router.use(authMiddleware);
+router.get('/me', getUser);
 
 export default router;
