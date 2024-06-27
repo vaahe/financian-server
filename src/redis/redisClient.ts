bc6c0fcb8945907fb747b63f9d349ba6c069fb73
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-06-28 01:40:26 +0400

    create redis service

---
+++ src/redis/redisClient.ts
@@ -0,0 +1,9 @@
+import Redis from 'ioredis';
+
+const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    password: process.env.REDIS_PASSWORD
+});
+
+export default redis;
