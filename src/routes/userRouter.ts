94424d4fcbe7bea7ba82bfe82faa001eb4b84014
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-06-29 14:40:33 +0400

    add multer middleware

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,13 +1,14 @@
 import { createUser, deleteUser, getUser, getUsers, updateUser } from "../controllers/userController";
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { uploadFiles } from "../middlewares/uploadMiddleware";
 
 const router: Router = Router();
 
 router.get('/', getUsers);
 router.post('/', createUser);
-router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
+router.patch('/:id', uploadFiles, updateUser);
 
 router.use(authMiddleware);
 router.get('/me', getUser);
