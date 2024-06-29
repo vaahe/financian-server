ca508a48fc7587096d64731ff820618f303c4bff
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-06-29 17:36:06 +0400

    deleting code after verification

--- src/config/mailConfig.ts
+++ src/config/mailConfig.ts
@@ -8,7 +8,7 @@ export const generateVerificationCode = (): string => {
 export const setVerificationCode = async (email: string, code: string): Promise<void> => {
     const key = `verificationCode:${email}`;
     const expirationTime = 3600; // 1 hour in seconds
-
+    console.log(key, code);
     try {
         await redis.set(key, code, 'EX', expirationTime);
     } catch (error) {
@@ -18,18 +18,34 @@ export const setVerificationCode = async (email: string, code: string): Promise<
 }
 
 export const getVerificationCode = async (email: string): Promise<string | null> => {
-    const key = `verificaitonCode:${email}`;
+    const key = `verificationCode:${email}`;
 
     try {
         const value = await redis.get(key);
+        console.log(`Retrieving key: ${key}, Value: ${value}`);
         return value;
     } catch (error) {
         console.error(error);
         throw new Error('Internal server error');
     }
-
 }
 
+export const deleteVerificationCode = async (email: string): Promise<void> => {
+    const key = `verificationCode:${email}`;
+
+    try {
+        const result = await redis.del(key);
+        
+        if (result === 0) {
+            console.warn(`No verification code found for email: ${email}`);
+        } else {
+            console.log(`Verification code for ${email} deleted successfully`);
+        }
+    } catch (error) {
+        console.error(error);
+        throw new Error("Internal server error");
+    }
+}
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
     let transporter = nodemailer.createTransport({
