05010b0b12a6e49bd5ef24c5dd8f2dd788b41466
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-07-21 03:08:31 +0400

    implement payment routes

--- src/routes/paymentRouter.ts
+++ src/routes/paymentRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { createOrder, createPaymentIntent } from "../controllers/paymentController";
 import { validateCreateIntent, validateCreateOrder } from "../middlewares/validations/paymentValidation";
+import { capturePaypalOrder, createOrder, createPaymentIntent, createPaypalOrder } from "../controllers/paymentController";
 
 const router = Router();
 
-router.post('/', validateCreateIntent, createPaymentIntent);
-router.post('/order', validateCreateOrder, createOrder);
+router.post('/stripe', validateCreateIntent, createPaymentIntent);
+router.post('/stripe/order', validateCreateOrder, createOrder);
 // router.post('/webhook', stripeWebHook);
+
+router.post('/paypal/create-order', createPaypalOrder);
+router.post('/paypal/capture-order', capturePaypalOrder);
 
 export default router;
