e66e41d6d5632d4d756ff0e8c46120d0f4d4750f
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-07-21 03:06:10 +0400

    add admin middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -19,23 +19,23 @@ export const authMiddleware = (req: AuthenticateRequest, res: Response, next: Ne
     }
 
     try {
-        const jwtSecret: string = process.env.JWT_ACCESS_SECRET || '';
-
-        if (!jwtSecret) {
-            console.error('JWT secret is not defined');
-            return res.status(500).json({ message: 'Internal server error' });
-        }
-
-        console.log('Token:', token);
-        console.log('JWT Secret:', jwtSecret);
-
+        const jwtSecret = process.env.JWT_ACCESS_SECRET!;
         const decodedToken = jwt.verify(token, jwtSecret) as DecodedToken;
-        console.log({ decodedToken });
-        req.user = decodedToken;
 
+        req.user = decodedToken;
         next();
     } catch (error) {
         console.error('Token verification failed:', error);
         return res.status(403).json({ message: 'Forbidden: Invalid token' });
+    }
+};
+
+export const checkAdmin = (req: AuthenticateRequest, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (user && user.userId === '9743c77d-e290-40c0-896a-973d8d05ab41') {
+        next();
+    } else {
+        res.status(403).json({ message: 'Forbidden: You do not have the required permissions' });
     }
 };
