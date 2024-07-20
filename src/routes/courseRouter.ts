8f721644d3e8bca39d07a6dd549f06ab4386609d
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-07-21 03:08:05 +0400

    add middlewares

--- src/routes/courseRouter.ts
+++ src/routes/courseRouter.ts
@@ -3,13 +3,16 @@ import express from 'express';
 import { uploadFiles } from '../middlewares/uploadMiddleware';
 import { createCourse, deleteCourse, getCourseById, getCourses, updateCourse } from '../controllers/courseController';
 import { validateCreateCourse, validateDeleteCourse, validateGetCourse, validateUpdateCourse } from '../middlewares/validations/courseValidation';
+import { authMiddleware, checkAdmin } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
+router.use(authMiddleware);
 router.get('/', getCourses);
-router.post('/', uploadFiles, validateCreateCourse, createCourse);
-
 router.get('/:id', validateGetCourse, getCourseById);
+
+router.use(checkAdmin);
+router.post('/', uploadFiles, validateCreateCourse, createCourse);
 router.patch(':id', validateUpdateCourse, updateCourse);
 router.delete('/:id', validateDeleteCourse, deleteCourse);
 
