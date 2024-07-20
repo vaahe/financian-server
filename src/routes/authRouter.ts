36cd7c4ced8f54bd6d7e575f42150fa7ab702929
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-07-21 03:07:53 +0400

    add middlewares

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 
-import { validate } from "../middlewares/validationMiddleware";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { logOut, signIn, signUp, verify } from "../controllers/authController";
+import { logOut, refreshToken, requestVerification, signIn, signUp, verify } from "../controllers/authController";
 import { loginRateLimit, validateSignIn, validateSignUp, validateVerify } from "../middlewares/validations/authValidation";
 
 const router: Router = Router();
 
 router.post('/signup', validateSignUp, signUp);
-router.post('/signin', validateSignIn, loginRateLimit, signIn);
-
 router.post('/verify', validateVerify, verify);
+router.post('/signin', validateSignIn, loginRateLimit, signIn);
 router.post('/logout', authMiddleware, logOut);
+router.post('/refresh', authMiddleware, refreshToken);
+router.post('/request-verification', requestVerification);
 
 export default router;
